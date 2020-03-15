8f98b3a2af1925ee4bf7167bf741a53dcfe108ed
yuma <yumai6205@gmail.com>
2020-03-15 22:30:13 +0900

    ヘッダー

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-btn text to="#">ジャズ研バンドエントリーシステム</v-btn>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
