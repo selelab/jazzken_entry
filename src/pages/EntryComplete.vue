958106914da5bacfef88676d7189968548f34d81
yuma <yumai6205@gmail.com>
2020-03-16 01:23:34 +0900

    完了画面

---
+++ src/pages/EntryComplete.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="5">
+        <v-card-text>
+          <h1 style="text-align: center">登録完了</h1>
+          <br />
+          <p style="text-align: center">情報を送信しました。</p>
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
