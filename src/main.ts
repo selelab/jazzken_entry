9288fc7a6016ced9ebb1b00cec8288a3d6d2667a
yuma <yumai6205@gmail.com>
2020-03-15 22:29:24 +0900

    vue作成

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import VueCompostionApi from '@vue/composition-api'
+import './registerServiceWorker'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.use(VueCompostionApi)
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount("#app");
+  vuetify,
+  render: h => h(App),
+}).$mount('#app')
