6a2dd3fb87b02f58772be28490c3bfe57bf92044
yuma <yumai6205@gmail.com>
2020-03-29 23:04:04 +0900

    ボタンの行き先

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,16 +1,39 @@
 <template>
   <v-app-bar app color="primary" dark>
     <div class="d-flex align-center">
-      <v-btn text href="http://abehiroshi.la.coocan.jp/" target="_brank"
-        >ジャズ研バンドエントリーシステム</v-btn
-      >
+      <v-btn text to="/">ジャズ研バンドエントリーシステム</v-btn>
     </div>
 
     <v-spacer></v-spacer>
 
-    <v-btn to="./list" text>
+    <v-btn v-if="path !== '/list'" to="./list" text>
       <span class="mr-2">バンド一覧</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+    <v-btn v-else to="/" text>
+      <span class="mr-2">バンドエントリー</span>
       <v-icon>mdi-open-in-new</v-icon>
     </v-btn>
   </v-app-bar>
 </template>
+
+<script lang="ts">
+import { defineComponent, SetupContext, ref, watch } from '@vue/composition-api'
+
+export default defineComponent({
+  setup(_, context: SetupContext) {
+    const path: any = ref(null)
+
+    watch(
+      () => context.root.$route.path,
+      newPath => {
+        path.value = newPath
+      }
+    )
+
+    return {
+      path,
+    }
+  },
+})
+</script>
