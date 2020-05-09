24f222f7e6f82ee8dcbf2946550749d636745622
yuma <yumai6205@gmail.com>
2020-05-09 18:51:56 +0900

    コンプリート画面からトップ画面に移動するボタンの追加

--- src/pages/EntryComplete.vue
+++ src/pages/EntryComplete.vue
@@ -3,6 +3,7 @@
     <v-row justify="center">
       <v-card-title>登録完了</v-card-title>
       <v-card-text style="text-align: center">情報を送信しました。</v-card-text>
+      <v-btn to="/" outlined>エントリーを続ける</v-btn>
     </v-row>
   </v-container>
 </template>
