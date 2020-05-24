afc1aff12f5174ab715b3a71b5e8368e75508072
yuma <yumai6205@gmail.com>
2020-05-24 22:34:00 +0900

    楽器リスト

---
+++ src/instrumentList.ts
@@ -0,0 +1,17 @@
+const instrumentList = [
+  'Vo',
+  'T.Sax',
+  'A.Sax',
+  'Fl',
+  'Cl',
+  'Tp',
+  'Tb',
+  'Gt',
+  'Pf',
+  'Ba',
+  'Dr',
+  'Perc',
+  'その他',
+]
+
+export { instrumentList }
